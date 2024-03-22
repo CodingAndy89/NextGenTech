5fff5f16520ae46e9d41b363a98b9ca930bdfedd
Andrew Kennedy <kennedyandrew33@gmail.com>
2024-03-22 03:26:16 -0700

    phase one

---
+++ src/products.js
@@ -0,0 +1,52 @@
+import product1 from "./assets/ASRock Intel Arc A770 Phantom Gaming D 8GB OC(L1).png";
+import product2 from "./assets/X570 Taichi Razer Edition(M1).png";
+import product3 from "./assets/h732.png";
+import product4 from "./assets/Asus ROG Strix B650E E Gaming Wifi.png";
+import product5 from "./assets/Asus ROG Strix z790 E Gaming Wifi.png";
+import product6 from "./assets/Gigabyte B650 Aorus Elite AX.png";
+import product7 from "./assets/i5-12600K.png";
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "ASRock Phantom Intel ARC A770",
+        price: 297.93,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: "ASRock x570 Taichi Razer Edition",
+        price: 235.99,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: "Asus ROG Maximus z790 Hero",
+        price: 399.99,
+        productImage: product3,
+    },
+    {
+        id: 4,
+        productName: "Asus ROG Strix B650E E Gaming Wifi",
+        price: 297.93,
+        productImage: product4,
+    },
+    {
+        id: 5,
+        productName: "Asus ROG Strix z790 E Gaming Wifi",
+        price: 380.86,
+        productImage: product5,
+    },
+    {
+        id: 6,
+        productName: "Gigabyte B650 Aorus Elite AX",
+        price: 199.99,
+        productImage: product6,
+    },
+    {
+        id: 7,
+        productName: "Intel i5-12600K",
+        price: 187.99,
+        productImage: product7,
+    },
+];
