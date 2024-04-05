603ae17fd479ab663c78a29a22440d334da670b3
Andrew Kennedy <kennedyandrew33@gmail.com>
2024-04-04 23:12:07 -0700

    ContactPage

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "bootstrap-social/bootstrap-social.css";
+import "font-awesome/css/font-awesome.css"
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
